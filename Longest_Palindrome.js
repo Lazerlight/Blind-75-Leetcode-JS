3ee14ff46b8a9d0f5934034190d06eea3d55ec41
Alexandar Slavov <107650563+Lazerlight@users.noreply.github.com>
2023-03-30 17:32:41 +0300

    Create Longest_Palindrome.js

---
+++ Longest_Palindrome.js
@@ -0,0 +1,33 @@
+/* Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here. */
+
+// My Solution 
+
+var longestPalindrome = function (s) {
+  let array = Array.from(s);
+  let map = new Map();
+  let extraChar = 0;
+  let palindrome = 0;
+
+  array.forEach((e) => {
+    let countChars = map.get(e) || 0;
+    if (array.includes(e)) {
+      map.set(e, countChars + 1);
+    }
+  });
+
+  for (let i = 0; i < map.size; i++) {
+    let e = Array.from(map.values())[i];
+    if (!(e % 2)) {
+      palindrome += e;
+    } else if (e % 2) {
+      palindrome += e - 1;
+      if (extraChar < 1) {
+        palindrome++;
+        extraChar++;
+      }
+    }
+  }
+  return palindrome;
+};
