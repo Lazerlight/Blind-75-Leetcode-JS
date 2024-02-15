0d885261c4b5e503c65d9f87e7cf2fa25b3c0790
Alexandar Slavov <107650563+Lazerlight@users.noreply.github.com>
2024-02-15 20:45:04 +0200

    Update Valid_Palindrome.js

--- Valid_Palindrome.js
+++ Valid_Palindrome.js
@@ -5,16 +5,7 @@ Given a string s, return true if it is a palindrome, or false otherwise. */
 // My Solution: 
 
 var isPalindrome = function (s) {
-  try {
-    let Arr = s.match(/[A-Za-z0-9]/g);
-    let filteredString = Arr.join("").toLowerCase();
-    let reverseString = Arr.reverse().join("").toLowerCase();
-    if (reverseString === filteredString) {
-      return true;
-    }
-    
-    return false;
-  } catch (exception) {
-    return true;
-  }
+  const original = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversed = original.split("").reverse().join("");
+  return original === reversed;
 };
